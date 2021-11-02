04f765dab8f9c3e4beb1465054e0051396f5c477
mschultz5200 <80858050+mschultz5200@users.noreply.github.com>
2021-11-02 11:12:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import search
 import pipeline
+import website_cleaner
+
 
 def main():
     webpage = search.search()
-    pipeline.handoff(webpage)
+    raw_data = pipeline.handoff(webpage)
+    website_cleaner.container(raw_data)
 
 
 main()
